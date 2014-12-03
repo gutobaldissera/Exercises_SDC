5f52d0a19f2246aaadfa52f557f6af63b587ce1d
Gustavo Baldissera <g.i.bondarenko@gmail.com>
2014-12-03 15:53:51 +0100

    test function for is_odd written

--- Ffile.py
+++ Ffile.py
@@ -1,17 +1,17 @@
 import sys
 import numpy as np
 
-data=sys.argv[1:]
-print data
+data=sys.argv[1:] #read the command line input 
 
-arr=np.array([])
+arr=np.array([]) #create a empty array
 
-for a in data:
-  arr=np.append(arr,float(a))
+for a in data:   
+  arr=np.append(arr,float(a)) #append every value to array
  
-
 print arr
 print type(arr)
+print len(arr)
+
 
 
 
--- test.py
+++ test.py
@@ -2,4 +2,10 @@ def test_is_integer():
 	assert is_integer(1.5) == False ,         'fail integer number function' 
 	assert is_integer(1.0) == True ,          'fail integer number function' 
 
+def test_is_odd()
+        assert is_odd(2) == False  , 'fail is_odd function, for even'
+        assert is_odd(1) == True   , 'fail, is_odd function, for odd'
+        assert is_odd(-1) == False , 'fail, is_odd function, negative number'
+	assert is_odd(3.3)== False , 'fail, is_odd function, float number'
+
 
